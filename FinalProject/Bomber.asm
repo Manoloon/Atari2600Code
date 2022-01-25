639893c423e29150e55ecdce5ca9e084cf4d12bd
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2022-01-25 18:09:57 -0300

    bomber y plane added

--- FinalProject/Bomber.asm
+++ FinalProject/Bomber.asm
@@ -10,10 +10,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	seg.u Variables
     org $80
-P0PosX 	byte
-P0PosY	byte
-P1PosX	byte
-P1PosY	byte
+P0PosX 		byte
+P0PosY		byte
+P1PosX		byte
+P1PosY		byte
+P0SprPtr	word
+P0ColPtr	word
+P1SprPtr	word
+P1ColPtr	word
+
+P0_HEIGHT =	9
+P1_HEIGHT =	9
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Start our ROM code segment starting at $F000.
@@ -29,11 +36,38 @@ reset:
 ;; Initialize variables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-	lda #10
-	sta P0PosX
 	lda #60
-	sta P0PosY 
+		sta P0PosX
+	lda #10
+		sta P0PosY 
+    lda #54
+       	sta P1PosX
+    lda #83
+		sta P1PosY
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Initialize pointers to the correct lookup table addresses
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+    lda #<P0Sprite
+    	sta P0SprPtr
+    lda #>P0Sprite
+    	sta P0SprPtr+1
+                
+    lda #<P1Sprite
+      	sta P1SprPtr
+    lda #>P1Sprite
+      	sta P1SprPtr+1
+                
+    lda #<P0Color
+       	sta P0ColPtr
+    lda #>P0Color
+      	sta P0ColPtr+1
+                
+    lda #<P1Color
+       	sta P1ColPtr
+    lda #>P1Color
+       	sta P1ColPtr+1
         
 NextFrame:
 
@@ -59,33 +93,60 @@ NextFrame:
     lda #0
 		sta VBLANK
         
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Start 192 lines visibles
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start 96 lines visibles --> Using 2-line Kernel
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
 VisibleLineLoop:
 
 	lda #$84
 		sta COLUBK
-	lda #$C2
+	lda #$C6
 		sta COLUPF
 	lda #%00000001
 		sta CTRLPF
 	lda #$F0
-		sta PF0				; enable reflect for the green part.
-
+		sta PF0			; enable reflect for the green part.
 	lda #$FC 
 		sta PF1
 	lda #0
 		sta PF2
 
-	ldx #192
+	ldx #96
 .GameLineLoop:
-	sta WSYNC
+.CheckP0Bounds:
+	txa
+    sec
+    sbc P0PosY
+    cmp P0_HEIGHT
+    bcc .DrawP0Sprite
+    lda #0
+.DrawP0Sprite:
+	tay
+    lda (P0SprPtr),y
+    	sta WSYNC
+    	sta GRP0
+    lda (P0ColPtr),y
+        sta COLUP0
+.CheckP1Bounds:
+	txa
+	sec
+    sbc P1PosY
+    cmp P1_HEIGHT
+    bcc .DrawP1Sprite
+    lda #0
+.DrawP1Sprite:
+	tay
+    lda #%0000101
+        sta NUSIZ1
+    lda (P1SprPtr),y
+        sta WSYNC
+        sta GRP1
+    lda (P1ColPtr),y
+        sta COLUP1
+        
 	dex
-	bne .GameLineLoop
-	
-       	
+	bne .GameLineLoop   	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Overscan
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -132,7 +193,37 @@ NoInput:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Lookup table for the player graphics bitmap
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-P0bmp:
+
+P0Sprite:
+	.byte %00000000
+        .byte #%00011100;$AA	   ###
+        .byte #%00001000;$0E		#
+        .byte #%01111111;$A8	 #######
+        .byte #%00111110;$0A      ##### 
+        .byte #%00011100;$0C       ###
+        .byte #%00001000;$AC        #
+        .byte #%00001000;$08        #
+        .byte #%00001000;$0E        #
+P0SpriteRight:
+        .byte #%00001100;$AA
+        .byte #%00001000;$0E
+        .byte #%00001111;$A8
+        .byte #%00001110;$0A
+        .byte #%00001100;$0C
+        .byte #%00001000;$AC
+        .byte #%00001000;$08
+        .byte #%00001000;$0E
+P0SpriteLeft:
+        .byte #%00011000;$AA
+        .byte #%00001000;$0E
+        .byte #%01111000;$A8
+        .byte #%00111000;$0A
+        .byte #%00011000;$0C
+        .byte #%00001000;$AC
+        .byte #%00001000;$08
+        .byte #%00001000;$0E
+           
+P1Sprite:
         .byte %00000000
         .byte %00001000
         .byte %00011100
@@ -142,20 +233,21 @@ P0bmp:
         .byte %00111110
         .byte %01111111
         .byte %00101010
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Lookup table for the player colors
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 P0Color:
-    	byte #$00
-    	byte #$40
-    	byte #$40
-    	byte #$40
-    	byte #$40
-    	byte #$42
-    	byte #$42
-    	byte #$44
-    	byte #$d2
-        
+        .byte #$AA;
+        .byte #$0E;
+        .byte #$A8;
+        .byte #$0A;
+        .byte #$0C;
+        .byte #$AC;
+        .byte #$08;
+        .byte #$0E;
+
 P1Color:
     	byte #$00
     	byte #$60
